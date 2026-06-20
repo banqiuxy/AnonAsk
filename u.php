588ded9874c01f24ec6378a0e03ee269c116b830
banqiuxy <1708241829@qq.com>
2026-06-20 18:15:01 +0800

    test

--- u.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * 用户公开页路由 — /u.php?uid=202400000001
- * 
- * 希望用 /u/{uid} 格式请配置 Nginx rewite:
- *   rewrite ^/u/(\d+)$ /u.php?uid=$1 last;
- */
-$_GET['uid'] = (int)($_GET['uid'] ?? 0);
-require __DIR__ . '/pages/u.php';
